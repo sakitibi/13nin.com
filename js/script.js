e88edcac51472ed5bd4bdd52b66e36a6dba598fc
さきちび <sakitibi10nin@icloud.com>
2025-04-03 18:14:20 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,3 @@ function openPopupzip() {
 function openPopup7z() {
   window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/4dd55279-eec9-761c-39b4-ec0afa14336f/", "popupWindow", "width=800,height=600");
 }
-
-document.getElementById("dl").addEventListener("click", function() {
-    location.href = "https://sakitibi.github.io/12nin.com/SKNewRoles.html";
-});
