781b18e0a1db514599e4c457dd9504b92aaeb9bc
さきちび <sakitibi10nin@icloud.com>
2025-04-06 18:00:23 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,10 +6,18 @@ document.getElementById('SNWInstall-7z').addEventListener('click', function() {
     openPopup7z();
     // Add your game start logic here
     });
+document.getElementById('SNWInstall-gz').addEventListener('click', function() {
+    openPopupgz();
+    // Add your game start logic here
+    });
 function openPopupzip() {
   window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/16412cdd-df6d-2d96-78e6-85da9563bf5e/", "popupWindow", "width=800,height=600");
 }
 
 function openPopup7z() {
   window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/4dd55279-eec9-761c-39b4-ec0afa14336f/", "popupWindow", "width=800,height=600");
+}
+
+function openPopupgz() {
+  window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/30f2c323-1373-18da-344b-f88b5dfad4c7/", "popupWindow", "width=800,height=600");
 }
