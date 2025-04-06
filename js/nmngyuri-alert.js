0298c700c917af586b8eccbaae6e84ed5a78f5b2
さきちび <sakitibi10nin@icloud.com>
2025-04-06 12:32:41 +0900

    Update nmngyuri-alert.js

--- js/nmngyuri-alert.js
+++ js/nmngyuri-alert.js
@@ -1,5 +1,10 @@
 window.setInterval(Alert, 5000);
 
-function Alert(){
-    alert("名前は長い方が有利戦争の応戦にご協力をお願い致します。");
-}
+let count = 5;
+alert("名前は長い方が有利戦争の応戦にご協力をお願い致します。").addEventListener('click', function(){
+    window.setInterval(function(){
+        console.log(count + '秒経過');
+        alert("名前は長い方が有利戦争の応戦にご協力をお願い致します。");
+        count += 5;
+    }, 5000);
+});
