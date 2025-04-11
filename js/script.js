0a4037e9554e1d8f2fa14be872df8a57a518f453
さきちび <sakitibi10nin@icloud.com>
2025-04-12 07:11:03 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 document.getElementById('SNWInstall-zip').addEventListener('click', function() {
     openPopupzip();
     // Add your game start logic here
-    });
+});
 document.getElementById('SNWInstall-7z').addEventListener('click', function() {
     openPopup7z();
     // Add your game start logic here
-    });
+});
 document.getElementById('SNWInstall-gz').addEventListener('click', function() {
     openPopupgz();
     // Add your game start logic here
-    });
+});
+document.getElementById('SNWInstall-xz').addEventListener('click', function() {
+    openPopupxz();
+    // Add your game start logic here
+});
+document.getElementById('SNWInstall-bzip2').addEventListener('click', function() {
+    openPopupbzip2();
+    // Add your game start logic here
+});
 function openPopupzip() {
   window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/16412cdd-df6d-2d96-78e6-85da9563bf5e/", "popupWindow", "width=800,height=600");
 }
@@ -20,4 +28,12 @@ function openPopup7z() {
 
 function openPopupgz() {
   window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/30f2c323-1373-18da-344b-f88b5dfad4c7/", "popupWindow", "width=800,height=600");
+}
+
+function openPopupgz() {
+  window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/30f2c323-1373-18da-344b-f88b5dfad4c7/", "popupWindow", "width=800,height=600");
+}
+
+function openPopupbzip2() {
+  window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/ec1ed140-6e8c-390d-e6f6-06efb3cc1ced/", "popupWindow", "width=800,height=600");
 }
