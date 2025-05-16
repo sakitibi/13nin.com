c6a1a103711d4e1cf394189947f555eca7833a4d
さきちび <sakitibi10nin@icloud.com>
2025-05-16 22:02:18 +0900

    Update elibrary_connect.js

--- SKNewRoles/elibrary_connect.js
+++ SKNewRoles/elibrary_connect.js
@@ -1,4 +1,3 @@
 	// ページとゲームの変数をeライブラリに保存
 	import * as ElibraryAPI from "https://sakitibi.github.io/12nin.com/elibrary/connect.min.js";
-	let SKNewRolesAnalytics = data;
 	export { SKNewRolesAnalytics }
