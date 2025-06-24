e2875c5f6125eaf1f12efde7619de0dcdbaaaff3
さきちび <sakitibi10nin@icloud.com>
2025-06-24 22:56:56 +0900

    Update connect.js

--- elibrary/connect.js
+++ elibrary/connect.js
@@ -5,7 +5,7 @@ setInterval(()=>{
 let copyright = document.getElementById("copyright")
 let osusume = document.getElementsByClassName("osusume")
 let date = new Date().getFullYear()
-let osusumerandom = Math.floor(Math.random() * 6);
+let osusumerandom = Math.floor(Math.random() * 7);
 
 copyright.innerHTML=`${date}`
 
@@ -22,5 +22,6 @@ for(let i=0; i < osusume.length; i++){
     case 3: osusume[i].innerHTML="ダブルキラー"; break;
     case 4: osusume[i].innerHTML="イビルゲッサー"; break;
     case 5: osusume[i].innerHTML="イビルシーア"; break;
+    case 6: osusume[i].innerHTML="コミュナー"; break;
   }
 }
