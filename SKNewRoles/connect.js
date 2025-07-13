d077c95d7bf00719faf88206de17a0cf55b29f84
さきちび <sakitibi10nin@icloud.com>
2025-07-13 12:28:06 +0900

    Update connect.js

--- SKNewRoles/connect.js
+++ SKNewRoles/connect.js
@@ -7,10 +7,12 @@
 	if (Counter === undefined){
 	    Counter = 0;
 	    localStorage.setItem("counter", Counter);
+	    document.cookie = `counter=${Counter}`;
 	}
 	for (let i = 0; i < SKNewRolesAnalytics.length; i++) {
 	    SKNewRolesAnalytics[i].addEventListener("click", function() {
 		Counter+=1;
 		localStorage.setItem("counter", Counter);
+		document.cookie = `counter=${Counter}`;
 	    });
 	}
