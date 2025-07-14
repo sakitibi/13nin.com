9943fee0b607788d6360c21f69f140c1c690b2f7
さきちび <sakitibi10nin@icloud.com>
2025-07-14 15:53:22 +0900

    Create download.js

---
+++ SKNewRoles/download.js
@@ -0,0 +1,39 @@
+document.getElementById('SNWInstall-zip').addEventListener('click', function() {
+    openPopupzip();
+    // Add your game start logic here
+});
+document.getElementById('SNWInstall-7z').addEventListener('click', function() {
+    openPopup7z();
+    // Add your game start logic here
+});
+document.getElementById('SNWInstall-gz').addEventListener('click', function() {
+    openPopupgz();
+    // Add your game start logic here
+});
+document.getElementById('SNWInstall-xz').addEventListener('click', function() {
+    openPopupxz();
+    // Add your game start logic here
+});
+document.getElementById('SNWInstall-bzip2').addEventListener('click', function() {
+    openPopupbzip2();
+    // Add your game start logic here
+});
+function openPopupzip() {
+  window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/51c52b03-3c5e-b59f-f6da-646e1b0671e6", "popupWindow", "width=800,height=600");
+}
+
+function openPopup7z() {
+  window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/f974b8cb-1ac4-582e-c7fa-bf6e4740e3bd", "popupWindow", "width=800,height=600");
+}
+
+function openPopupgz() {
+  window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/820c9725-4c8f-2606-8f63-b9e62e48d3e2", "popupWindow", "width=800,height=600");
+}
+
+function openPopupxz() {
+  window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/cf1e5b7f-adba-d67b-5249-3da4c61b3b7b", "popupWindow", "width=800,height=600");
+}
+
+function openPopupbzip2() {
+  window.open("https://sakitibi-com9.webnode.jp/api/sknewroles/login/11f5cb71-ea20-50e0-05ed-50b08d8efb33", "popupWindow", "width=800,height=600");
+}
