81162683944bca0caf19640eb90e2738e5968bff
さきちび <sakitibi10nin@icloud.com>
2025-11-16 13:45:33 +0900

    Update 2c85a09a-9453-cfdf-f32f-682aed35949f.js

--- elibrary/2c85a09a-9453-cfdf-f32f-682aed35949f.js
+++ elibrary/2c85a09a-9453-cfdf-f32f-682aed35949f.js
@@ -198,42 +198,135 @@
                 document.getElementById("skys-witch").addEventListener("click", function(){
                     etaMessageRandom = Math.floor(Math.random() * 8);
                     message1.innerHTML = '<img src="../enemys/witch/skyswitch.png" width="400" height="400" onselect="return false" onmousedown="return false">';
-		    if(etaMessageRandom === 0){
-			message2.innerHTML = (`
-   			    <p>魔法で空を飛行するわ</p>
-	  		    <p>ヒェーッヒェッヒェッヒェッ、</p>
-   			`);
-		    } else if(etaMessageRandom === 1){
-			message2.innerHTML = (`
-   			    <p>あなたから魔法でHPを奪い取ったわ</p>
-	  		    <p>ヒェーッヒェッヒェッヒェッ、</p>
-   			`);
-		    } else if(etaMessageRandom === 2){
-			message2.innerHTML = (`
-   			    <p>姿暗呪術で透明になって姿を消しましたわ</p>
-	  		    <p>ヒェーッヒェッヒェッヒェッ、</p>
-   			`);
-		    } else if(etaMessageRandom === 3){
-			message2.innerHTML = (`
-   			    <p>あなたを魔法で空に飛ばしたわ</p>
-	  		    <p>ヒェーッヒェッヒェッヒェッ、</p>
-   			`);
-		    } else if(etaMessageRandom === 4){
-			message2.innerHTML = (`
-   			    <p>あなたを呪術で眠らして操りますわ</p>
-	  		    <p>ヒェーッヒェッヒェッヒェッ、</p>
-   			`);
-		    } else if(etaMessageRandom === 5){
-			message2.innerHTML = (`
-   			    <p>あなたの視界を呪術で奪いますわ</p>
-	  		    <p>ヒェーッヒェッヒェッヒェッ、</p>
-   			`);
-		    } else if(etaMessageRandom === 6){
-			message2.innerHTML = (`
-   			    <p>あなたを眠操呪術で操ったわ</p>
-	  		    <p>ヒェーッヒェッヒェッヒェッ、</p>
-   			`);
-		    } else {
-			message2.innerHTML = "<p>あなたを空から見つけたわよ、ヒェーッヒェッヒェッヒェッ、</p>";
-		    }
-                });
+					if(etaMessageRandom === 0){
+						message2.innerHTML = (`
+							<p>魔法で空を飛行するわ</p>
+							<p>ヒェーッヒェッヒェッヒェッ、</p>
+						`);
+					} else if(etaMessageRandom === 1){
+						message2.innerHTML = (`
+							<p>あなたから魔法でHPを奪い取ったわ</p>
+							<p>ヒェーッヒェッヒェッヒェッ、</p>
+						`);
+					} else if(etaMessageRandom === 2){
+						message2.innerHTML = (`
+							<p>姿暗呪術で透明になって姿を消しましたわ</p>
+							<p>ヒェーッヒェッヒェッヒェッ、</p>
+						`);
+					} else if(etaMessageRandom === 3){
+						message2.innerHTML = (`
+							<p>あなたを魔法で空に飛ばしたわ</p>
+							<p>ヒェーッヒェッヒェッヒェッ、</p>
+						`);
+					} else if(etaMessageRandom === 4){
+						message2.innerHTML = (`
+							<p>あなたを呪術で眠らして操りますわ</p>
+							<p>ヒェーッヒェッヒェッヒェッ、</p>
+						`);
+					} else if(etaMessageRandom === 5){
+						message2.innerHTML = (`
+							<p>あなたの視界を呪術で奪いますわ</p>
+							<p>ヒェーッヒェッヒェッヒェッ、</p>
+						`);
+					} else if(etaMessageRandom === 6){
+						message2.innerHTML = (`
+							<p>あなたを眠操呪術で操ったわ</p>
+							<p>ヒェーッヒェッヒェッヒェッ、</p>
+						`);
+					} else {
+						message2.innerHTML = "<p>あなたを空から見つけたわよ、ヒェーッヒェッヒェッヒェッ、</p>";
+					}
+        		});
+		document.getElementById("water-witch").addEventListener("click", function(){
+			etaMessageRandom = Math.floor(Math.random() * 13);
+			message1.innerHTML = '<img src="../enemys/witch/water-witch.png" width="400" height="400" onselect="return false" onmousedown="return false">';
+			if(etaMessageRandom === 0){
+				message2.innerHTML = (`
+		            <p>ヒッヒッヒーッ、</p>
+		            <p>私は水の魔女</p>
+		            <p>土の魔女の実の妹ですわ</p>
+		            <p>水を操る魔法と</p>
+		            <p>少女に化けるのと</p>
+		            <p>姉と連携するのが得意ですわ</p>
+		            <p>あなたをを水や毒で命を奪いますわ</p>
+		            <p>ヒッヒッヒッ、</p>
+		        `);
+			} else if(etaMessageRandom === 1){
+				message2.innerHTML = (`
+					<p>いっけー!波魔法!</p>
+					<p>ヒッヒッヒーッ、</p>
+				`);
+			} else if(etaMessageRandom === 2){
+				message2.innerHTML = (`
+					<p>いっけー!波魔法!</p>
+					<p>ヒッヒッヒーッ、</p>
+					<p>あなたは女子だから波魔法耐性が無いわね〜</p>
+					<p>ヒーッヒッヒッヒーッ、</p>
+				`);
+			} else if(etaMessageRandom === 3){
+				message2.innerHTML = (`
+					<p>渦よ!あなたを包めー!</p>
+					<p>ヒッヒッヒーッ、</p>
+				`);
+			} else if(etaMessageRandom === 4){
+				message2.innerHTML = (`
+					<p>渦よ!${username}を包めー!</p>
+					<p>ヒッヒッヒーッ、</p>
+					<p>あなたは女子だから渦魔法耐性が無いわね〜</p>
+					<p>ヒーッヒッヒッヒーッ、</p>
+				`);
+			} else if(etaMessageRandom === 5){
+				message2.innerHTML = (`
+					<p>ヒッヒッヒッ、</p>
+					<p>水の魔女の杖を強化したわ</p>
+				`);
+			} else if(etaMessageRandom === 6){
+				message2.innerHTML = (`
+					<p>毒よ!あなたを蝕めー!</p>
+					<p>ヒッヒッヒーッ、</p>
+				`);
+			} else if(etaMessageRandom === 7){
+				message2.innerHTML = (`
+					<p>毒よ!あなたを腐らせー!</p>
+					<p>ヒッヒッヒーッ、</p>
+					<p>あなたは女子だから腐食耐性が無いわね〜</p>
+					<p>これであなたを腐らせ放題</p>
+					<p>ヒーッヒッヒッヒーッ、</p>
+				`);
+			} else if(etaMessageRandom === 8){
+				message2.innerHTML = (`
+					<p>いっけー!熱水魔法!</p>
+					<p>ヒッヒッヒッ、</p>
+				`);
+			} else if(etaMessageRandom === 9){
+				message2.innerHTML = (`
+					<p>いっけー!熱水魔法!</p>
+					<p>ヒッヒッヒッ、</p>
+					<p>あなたは女子だから熱水耐性が無いわね〜</p>
+					<p>ヒーッヒッヒッヒーッ、</p>
+				`);
+			} else if(etaMessageRandom === 10){
+				message2.innerHTML = (`
+                    <p>水の魔女&土の魔女『</p>
+                    <p>もうすぐ二人であの恐怖の魔法をかけてやりますわ〜</p>
+                    <p>ヒーッヒッヒッヒーッ、』</p>
+                `);
+			} else if(etaMessageRandom === 11){
+				message2.innerHTML = (`
+			        <p>土の魔女「</p>
+			        <p>妹、準備はいい?</p>
+			        <p>それじゃー妹、いくわよ〜</p>
+			        <p>せーの!」<br/>土の魔女&水の魔女
+			            <span style="font-size: 30px; color: #640dd5">
+			                『必殺! <span style="color: #07cefa">熱水</span><span style="color: #875607">土</span>魔法!』
+			            </span>
+			        </p>
+			    `);
+			} else if(etaMessageRandom === 12){
+				message2.innerHTML = (`
+					<p>水の魔女&土の魔女</p>
+					<p>『あなたは女子だから熱水土耐性が無いわね〜</p>
+					<p>ヒーッヒッヒッヒーッ、』</p>
+                `);
+			}
+		});
