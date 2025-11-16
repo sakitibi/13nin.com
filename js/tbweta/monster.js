cb8f5456a1551c31c115b08e2855be37f4ec88c4
さきちび <sakitibi10nin@icloud.com>
2025-11-16 13:47:00 +0900

    Create monster.js

---
+++ js/tbweta/monster.js
@@ -0,0 +1,13 @@
+export default function monster(){
+  etaMessageRandom = Math.floor(Math.random() * 4);
+    message1.innerHTML = "";
+  if(etaMessageRandom === 0){
+    message2.innerHTML = '<p>自分がモンスターの中一番弱いんだってよ</p>';
+  } else if(etaMessageRandom === 1){
+    message2.innerHTML = '<p>うわ、プレイヤー強そう、</p>';
+  } else if(etaMessageRandom === 2){
+    message2.innerHTML = '<p>プレイヤー逃げてくれ、</p>';
+  } else {
+    message2.innerHTML = '<p>てかなんで自分らだけ画像が1つしか無いん</p>';
+  }
+}
