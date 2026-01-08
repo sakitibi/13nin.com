704fe7d62f793284539d5a4906ec2bf5b4de97a3
さきちび <sakitibi10nin@icloud.com>
2026-01-08 16:02:30 +0900

    Update connect.js

--- elibrary/connect.js
+++ elibrary/connect.js
@@ -2,27 +2,29 @@ setInterval(()=>{
   localStorage.getItem("counter");
 },50);
 
-let copyright = document.getElementById("copyright")
-let osusume = document.getElementsByClassName("osusume")
-let date = new Date().getFullYear()
-let osusumerandom = Math.floor(Math.random() * 8);
-
-copyright.innerHTML=`${date}`
-
-setInterval(()=>{
-  date=new Date().getFullYear()
+document.addEventListener("DOMContentLoaded", function(){
+  let copyright = document.getElementById("copyright");
+  let osusume = document.getElementsByClassName("osusume");
+  let date = new Date().getFullYear();
+  let osusumerandom = Math.floor(Math.random() * 8);
+  
   copyright.innerHTML=`${date}`
-},1e3);
-
-for(let i=0; i < osusume.length; i++){
-  switch (osusumerandom){
-    default: osusume[i].innerHTML="人狼"; break;
-    case 1: osusume[i].innerHTML="アサシン"; break;
-    case 2: osusume[i].innerHTML="リモコン"; break;
-    case 3: osusume[i].innerHTML="ダブルキラー"; break;
-    case 4: osusume[i].innerHTML="イビルゲッサー"; break;
-    case 5: osusume[i].innerHTML="イビルシーア"; break;
-    case 6: osusume[i].innerHTML="コミュナー"; break;
-    case 7: osusume[i].innerHTML="ポイゾナー"; break;
+  
+  setInterval(()=>{
+    date=new Date().getFullYear()
+    copyright.innerHTML=`${date}`
+  },1e3);
+  
+  for(let i=0; i < osusume.length; i++){
+    switch (osusumerandom){
+      default: osusume[i].innerHTML="人狼"; break;
+      case 1: osusume[i].innerHTML="アサシン"; break;
+      case 2: osusume[i].innerHTML="リモコン"; break;
+      case 3: osusume[i].innerHTML="ダブルキラー"; break;
+      case 4: osusume[i].innerHTML="イビルゲッサー"; break;
+      case 5: osusume[i].innerHTML="イビルシーア"; break;
+      case 6: osusume[i].innerHTML="コミュナー"; break;
+      case 7: osusume[i].innerHTML="ポイゾナー"; break;
+    }
   }
-}
+});
